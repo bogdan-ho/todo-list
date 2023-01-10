437ddee977555772d83f16bc9b012660c3a835dc
Bogdan <homoretsky@gmail.com>
2023-01-10 10:40:12 +0500

    update ci tests, playwright configs

--- playwright.config.js
+++ playwright.config.js
@@ -37,7 +37,7 @@ const config = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -99,10 +99,11 @@ const config = {
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: 'npm run start',
+    port: 3000,
+    timeout: 120000,
+  },
 };
 
 export default config;
